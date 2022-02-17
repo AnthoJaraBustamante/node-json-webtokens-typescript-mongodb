f4ff41a8da99a12ca4b26fce160acd446b9a643c
AnthoJaraBustamante <omarbush81@gmail.com>
2022-02-17 11:01:18 -0500

    .

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 mongoose
-  .connect(process.env.TOKEN_SECRET || "mongodb://localhost/test")
+  .connect(process.env.DATABASE || "mongodb://localhost/test")
   .then((db) => console.log("db is connected"))
   .catch((err) => console.log(err));
