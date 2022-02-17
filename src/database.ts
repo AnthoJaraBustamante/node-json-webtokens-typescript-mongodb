8fe4b8a075425c61c4355ae920186c11c2015dba
AnthoJaraBustamante <omarbush81@gmail.com>
2022-02-17 17:52:51 -0500

    .

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 mongoose
-  .connect(process.env.DATABASE || "mongodb+srv://ajarabustamante:Dned22dj@cluster0.mmyqi.mongodb.net/test")
+  .connect(process.env.DATABASE as string)
   .then((db) => console.log("db is connected"))
   .catch((err) => console.log("DATA BASE ERROR"));
